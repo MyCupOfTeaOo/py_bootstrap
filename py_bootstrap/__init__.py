890da6b1bf9daf8e2987257dbd8a475bee1921c4
zou wendi <542095059@qq.com>
2021-03-01 14:41:32 +0800

    feat: load_config 独立方法

--- py_bootstrap/__init__.py
+++ py_bootstrap/__init__.py
@@ -1,2 +1,5 @@
-from .config import config, get_app_homepage
+from .config import config, get_app_homepage, load_config
+
+load_config()
+
 __all__ = ('config', 'get_app_homepage')
--- py_bootstrap/config.py
+++ py_bootstrap/config.py
@@ -56,20 +56,6 @@ eureka_heart = config['eureka_heart']
 eureka = EurekaClient(app_name=app_name, port=port, ip_addr=ip,
                       eureka_url=eureka_url)
 
-config_app = eureka.get_app(config_server_name)
-config_instances = config_app['application']['instance']
-config_instance = random.choice(config_instances)
-url = '{homepage}{app_name}-{profile}{extra_profiles}.json'.format(
-    homepage=config_instance['homePageUrl'], app_name=app_name, profile=profile,
-    extra_profiles=f',{extra_profiles}' if extra_profiles else '')
-
-settings = requests.get(url).json()
-config.update(settings)
-with open('config.json', 'w', encoding='utf-8') as f:
-    f.write(json.dumps(config, ensure_ascii=False,
-                       indent=4, separators=(',', ':')))
-print('加载配置成功')
-
 
 def register_eureka():
     def heart():
@@ -109,5 +95,20 @@ def get_app_homepage(name, **kwargs):
     return config_instance['homePageUrl']
 
 
-if config.get('register', False):
-    register_eureka()
+def load_config():
+    config_app = eureka.get_app(config_server_name)
+    config_instances = config_app['application']['instance']
+    config_instance = random.choice(config_instances)
+    url = '{homepage}{app_name}-{profile}{extra_profiles}.json'.format(
+        homepage=config_instance['homePageUrl'], app_name=app_name, profile=profile,
+        extra_profiles=f',{extra_profiles}' if extra_profiles else '')
+
+    settings = requests.get(url).json()
+    config.update(settings)
+    with open('config.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(config, ensure_ascii=False,
+                           indent=4, separators=(',', ':')))
+
+    print('加载配置成功')
+    if config.get('register', False):
+        register_eureka()
